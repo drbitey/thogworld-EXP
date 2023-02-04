df8499b93fde8121873f521e8708388ecd101a41
drbitey <drbitey@gmail.com>
2023-02-04 13:28:19 -0600

    Create hl.js

---
+++ hl.js
@@ -0,0 +1,31 @@
+const text = document.querySelector('.text');
+
+text.innerHTML = text.innerHTML.split('').map(char => {
+  return `<span>${char}</span>`;
+}).join('');
+
+text.addEventListener('mouseover', e => {
+  if (e.target.tagName === 'SPAN') {
+    e.target.classList.add('highlight-red');
+    if (e.target.previousElementSibling) {
+      e.target.previousElementSibling.classList.add('highlight-blue');
+    }
+    if (e.target.nextElementSibling) {
+      e.target.nextElementSibling.classList.add('highlight-blue');
+    }
+  }
+});
+
+text.addEventListener('mouseout', e => {
+  if (e.target.tagName === 'SPAN') {
+    e.target.classList.remove('highlight-red');
+    if (e.target.previousElementSibling) {
+      e.target.previousElementSibling.classList.remove('highlight-blue');
+    }
+    if (e.target.nextElementSibling) {
+      e.target.nextElementSibling.classList.remove('highlight-blue');
+    }
+  }
+});
+
+
