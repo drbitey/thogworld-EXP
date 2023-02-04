e7086d7a838d49db2e4d5462ec250bcc4931989e
drbitey <drbitey@gmail.com>
2023-02-04 13:28:02 -0600

    Update highlight.js

--- highlight.js
+++ highlight.js
@@ -1,3 +1,21 @@
+const text = document.querySelector('.text');
+
+text.innerHTML = text.innerHTML.split('').map(char => {
+  return `<span>${char}</span>`;
+}).join('');
+
+text.addEventListener('mouseover', e => {
+  if (e.target.tagName === 'SPAN') {
+    e.target.classList.add('highlight-red');
+    if (e.target.previousElementSibling) {
+      e.target.previousElementSibling.classList.add('highlight-blue');
+    }
+    if (e.target.nextElementSibling) {
+      e.target.nextElementSibling.classList.add('highlight-blue');
+    }
+  }
+});
+
 text.addEventListener('mouseout', e => {
   if (e.target.tagName === 'SPAN') {
     e.target.classList.remove('highlight-red');
@@ -9,3 +27,5 @@ text.addEventListener('mouseout', e => {
     }
   }
 });
+
+
